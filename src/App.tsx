3d54adc574832b96e3d56e45746da5639b9f7d69
James Serengia <jamesserengia@gmail.com>
2023-10-29 06:26:06 +0300

    Explored different approaches of props Types annotation

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import CourseGoal from "./components/CourseGoal";
 export default function App() {
   return (
     <main>
-      <CourseGoal
-        title="React Typescript Goals App"
-        description="Create simple Goals App with React and Typescript"
-      />
+      <CourseGoal title="React Typescript Goals App">
+        <p>Create simple Goals App with React and Typescript</p>
+      </CourseGoal>
     </main>
   );
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,14 +1,32 @@
-type CourseGoalProps = {
-  title: string;
-  description: string;
-};
+import { type PropsWithChildren } from "react";
 
-export default function CourseGoal({ title, description }: CourseGoalProps) {
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
+
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
+
+export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
       <div>
         <h2>{title}</h2>
-        <p>{description}</p>
+        {children}
       </div>
       <button>Delete</button>
     </article>

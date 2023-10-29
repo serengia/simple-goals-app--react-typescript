5bf0c11214183b714a3ee250f3452f4ae4ba0a4f
James Serengia <jamesserengia@gmail.com>
2023-10-29 07:18:24 +0300

    Add react annotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
 import CourseGoal from "./components/CourseGoal";
 import Header from "./components/Header";
 
 export default function App() {
+  type Goal = {
+    title: string;
+    description: string;
+    id: number;
+  };
+
+  const [goals, setGoals] = useState<Goal[]>([]);
+
+  const addGoalHandler = () => {
+    setGoals((prev) => {
+      const newGoal: Goal = {
+        title: "Goal one",
+        description: "Goal one description",
+        id: Math.random() + Math.random(),
+      };
+      return [newGoal, ...prev];
+    });
+  };
+
   return (
     <main>
       <Header image={{ src: "/goals.jpg", alt: "Set your goals" }}>
         <h1>My Goals</h1>
       </Header>
-      <CourseGoal title="React Typescript Goals App">
-        <p>Create simple Goals App with React and Typescript</p>
-      </CourseGoal>
+      <button onClick={addGoalHandler}>Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
